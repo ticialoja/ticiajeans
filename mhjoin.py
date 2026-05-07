e7656ea4affabfda795f7e337b5588a1820b49f8
ticialoja <seuemail@gmail.com>
2026-05-07 14:32:50 -0300

    v4 final

---
+++ mhjoin.py
@@ -0,0 +1,13 @@
+import base64,os
+d=[]
+for i in range(1,9):
+    f=open(f"/data/data/com.termux/files/home/.mh{i}.txt")
+    d.append(f.read().replace("\n",""))
+    f.close()
+html=base64.b64decode("".join(d))
+out=open("/data/data/com.termux/files/home/tj2/markethub.html","wb")
+out.write(html)
+out.close()
+for i in range(1,9):
+    os.remove(f"/data/data/com.termux/files/home/.mh{i}.txt")
+print("PRONTO markethub!")
